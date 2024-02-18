a5877f9a75225451548212669057c844a5007b21
bnorikane <bnorikane@msn.com>
2024-02-18 07:23:46 -0700

    Update supersite data from Judi's final - data/2024_Supersite_list w Chairs & Cochairs-copy.xlsx

--- supersites.py
+++ supersites.py
@@ -4,13 +4,26 @@
 '''
 import pandas as pd
 
+# cols = ['CONFIRMED', 'APPROVED', 'CONTRACT FINAL', 'Supersite', 'AC Areas',
+#        'Region', 'SD', 'HD', '# of Reg Dems', 'Forecast of  Attendees',
+#        '# of Pct's', 'Pct #'s', 'PCT #'s', 'PCT #'s.1', 'PCT #'s.2',
+#        'PCT #'s.3', 'PCT #'s.4', 'PCT #'s.5', 'PCT #'s.6', 'PCT #'s.7',
+#        'PCT #'s.8', 'PCT #'s.9', 'PCT #'s.10', 'PCT #'s.11', 'PCT #'s.12',
+#        'PCT #'s.13', 'PCT #'s.14', 'PCT #'s.15', 'PCT #'s.16', 'PCT #'s.17',
+#        'PCT #'s.18', 'PCT #'s.19', 'PCT #'s.20', 'PCT #'s.21', 'PCT #'s.22',
+#        'PCT #'s.23', 'PCT #'s.24', '# of Chairs', 'Chair 1', 'Chair Name',
+#        'Phone', 'Email', 'Chair 2', 'Chair2 Name', 'Phone.1', 'Email.1',
+#        'Chair 3', 'Chair3 Name', 'Phone.2', 'Email.2', 'Chair 4',
+#        'chair 4 Name', 'Phone.3', 'eMail', 'Chair 1.1', 'Chair 2.1',
+#        'Chair 3.1', 'Chair 4.1', 'Unnamed: 58', 'Sort']
+
 def read_supersite_pct (ssfile, sheetname):
     return (
         pd.read_excel(ssfile, sheet_name=sheetname, skiprows=3)
         .rename(columns={
             "Supersite":"supersite", 
             "# of Reg Dems":"dems", 
-            "Fcst  Turn Out":"attendee_forecast", 
+            "Forecast of  Attendees":"attendee_forecast", 
             "# of Pct's":"total_precincts" 
             })
         .assign(pctlist = pctstr_to_list)
